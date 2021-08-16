495974fc48ee9042b5de1fa9c903c3868086cd88
namhla1221 <namhlamthi21@gmail.com>
2021-08-16 11:52:14 +0200

    new addition of work

--- src/components/App/App.Routing.jsx
+++ src/components/App/App.Routing.jsx
@@ -1,89 +1,20 @@
 import React, { useContext } from "react";
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, Redirect } from "react-router-dom";
 import { context as authContext } from '../../hooks/useAuth';
 
+import { Demos } from './App.Routing.Demo'
+import { Auth } from "./App.Routing.Auth";
+import { Create } from "./App.Routing.Create";
 
-import { Demo as ButtonDemo } from "../Button/Button.Demo";
-import { Demo as CheckboxDemo } from "../Checkbox/Checkbox.Demo";
-import { Demo as ImageDemo } from "../Image/Image.Demo";
-import { Demo as InputDemo } from "../Input/Input.Demo";
-import { Demo as LayoutDemo } from "../Layout/Layout.Demo";
-import { Demo as LinkDemo } from "../Link/Link.Demo";
-import { Demo as TextDemo } from "../Text/Text.Demo";
-import {Demo as AlertDemo} from "../Alert/Alert.Demo"
-
-import {LandingPage} from "../../views/LandingPage";
-import {NewAccount} from "../../views/NewAccount";
-import {ResetPassword} from "../../views/ResetPassword";
-import {SignIn} from "../../views/SignIn";
-import { EmailSent } from "../../views/EmailSent";
-import ItemsList from "../../views/ItemsList";
-
-const Demos = ()=>{
-  return (
-    <Switch>
-      <Route path="/demo/button">
-        <ButtonDemo />
-      </Route>
-
-      <Route path="/demo/checkbox">
-        <CheckboxDemo />
-      </Route>
-
-      <Route path="/demo/image">
-        <ImageDemo />
-      </Route>
-
-      <Route path="/demo/input">
-        <InputDemo />
-      </Route>
-
-      <Route path="/demo/layout">
-        <LayoutDemo />
-      </Route>
-
-      <Route path="/demo/link">
-        <LinkDemo />
-      </Route>
-
-      <Route path="/demo/text">
-        <TextDemo />
-      </Route>
-
-      <Route path="/demo/alert">
-        <AlertDemo />
-      </Route>
-    </Switch>
-  );
-}
-
-const Auth = ()=>{
-  return (
-    <Switch>
-      <Route path="/auth/register">
-        <NewAccount />
-      </Route>
-
-      <Route path="/auth/signin">
-        <SignIn />
-      </Route>
-
-      <Route path="/auth/reset">
-        <ResetPassword />
-      </Route>
-
-      <Route path="/auth/sent">
-        <EmailSent />
-      </Route>
-    </Switch>
-  );
-}
+import {LandingPage} from "../../views/general/LandingPage";
+import { EmailSent } from "../../views/general/EmailSent";
+import { ItemsList } from "../../views/ItemsList"
 
 const Items = () => {
   return (
     <Switch>
       <Route path="/items/list">
-        < ItemsList/>
+        <ItemsList />
       </Route>
       </Switch>
    
@@ -105,22 +36,33 @@ export const Routing = () => {
         </Route>
 
         <Route path="/items">
-          {user ? <Items /> :  <LandingPage />}
+          {user ? <Items /> :  <Redirect to="/" />}
         </Route>
 
         <Route path="/auth">
-        
-          { user ? <ItemsList /> : <Auth />}
+          { user ? <Redirect to="/items/list" /> : <Auth />}
+        </Route>
+
+        <Route path="/create">
+          { user ? <Redirect to="/items/list" /> : <Create />}
+        </Route>
+
+        <Route path="/sent">
+          <EmailSent />
         </Route>
 
         <Route path="/">
-        { user ? <ItemsList /> : <LandingPage />}
+        { user ? <Redirect to="/items/list" /> : <LandingPage />}
 
           
         </Route>
       </Switch>
     
   );
+
+
+
+
 };
 export default Routing;
 
---
+++ src/views/create/EnableSync/EnableSync.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const EnableSync = () => {
+    return <div>1231</div>
+}
+
+export default EnableSync;
